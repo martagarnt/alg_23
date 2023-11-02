8a69b5b2c24ee917bc332286c7f8afa8369ad249
martagarnt <you@example.com>
2023-11-02 15:24:39 +0100

    Revised version of peek.sh version 3.1

--- peek/peek.sh
+++ peek/peek.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-if [[ $(wc -l < "$1") -lt 6 ]]; then
+n_lines=3
+
+# checking if the lines are defined
+
+if [[ -z "$2" ]]; then
+    n="$n_lines"
+else
+    n="$2"
+fi
+
+# checking the lines
+
+lines_file=$(wc -l < "$1")
+
+# making the printing
+
+if [[ "$lines_file" -le $((2*n)) ]]; then
   cat "$1"
 else
-  head -n3 "$1"
+  echo "Warning: the output of this file will be only printing the $n first lines and the $n last lines "
+  head -n "$n" "$1"
   echo "..."
-  tail -n3 "$1"
+  tail -n "$n" "$1"
 fi
